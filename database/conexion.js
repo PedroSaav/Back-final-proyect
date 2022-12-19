2d5c6243c41540902e5b93627aa82845519227f4
Pedro <pepitosk8hd@gmail.com>
2022-12-19 19:39:53 -0300

    ahora si

---
+++ database/conexion.js
@@ -0,0 +1,23 @@
+const db_database = require('../config');
+const db_host = require('../config');
+const db_password = require('../config');       
+const db_port = require('../config');   
+const db_user = require('../config');
+const mysql = require('mysql2');
+
+// Database Connection
+ const conexion = mysql.createConnection({
+    host: db_host,
+    user: db_user,
+    password: db_password,
+    database: db_database,
+    port: db_port
+}); 
+
+conexion.connect((err) => {
+    if (err) {
+        console.error(`Error en la conexión: ${err.stack}`)
+        return;
+    }
+    console.log(`Conectado a la Base de Datos ${process.env.db_database}`);
+});  
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 require('dotenv').config();
 const path = require('path');
-const mysql = require('mysql2');
+const app = express()
+const PORT= 3001 || 8080
 // const nodemailer = require('nodemailer');
 const cors = require('cors');
 const corsOptions ={
@@ -9,32 +10,6 @@ const corsOptions ={
     credentials:true,
     optionSuccessStatus:200
 }
-
-const db_database = require('./config');
-const db_host = require('./config');
-const db_password = require('./config');        
-const db_port = require('./config');    
-const db_user = require('./config');
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-// Database Connection
- const conexion = mysql.createConnection({
-    host: db_host,
-    user: db_user,
-    password: db_password,
-    database: db_database,
-    port: db_port
-}); 
-
- conexion.connect((err) => {
-    if (err) {
-        console.error(`Error en la conexión: ${err.stack}`)
-        return;
-    }
-    console.log(`Conectado a la Base de Datos ${process.env.db_database}`);
-});  
 
 //Middelwares settings
 app.use(express.json());
